d065a0283323995d2f52189dd43fbba112319592
root <zaqthefreshman@gmail.com>
2014-03-10 22:38:14 +0000

    added support for sending arbitrary GET requests. will log to /tmp/postData.log will return results

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php $postdata = file_get_contents("php://input");
-file_put_contents("/tmp/postData.log", "[".date("Y-m-d H:i:s")."]".$postdata."\r\n", FILE_APPEND);
-echo $postdata;
+$headers = print_r(getallheaders(), true);
+if($postdata)
+{
+	file_put_contents("/tmp/postData.log", "[".date("Y-m-d H:i:s")."][".$postdata."][".$headers."]\r\n", FILE_APPEND);
+	echo $postdata;
+}
+else
+{
+	file_put_contents("/tmp/postData.log", "[".date("Y-m-d H:i:s")."][".print_r($_GET, true)."]".$headers."]\r\n", FILE_APPEND);
+	echo print_r($_GET, true);
+}
 ?>
